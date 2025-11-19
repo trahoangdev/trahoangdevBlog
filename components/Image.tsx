bdbc1c4fde63c145f5aad6bf931142c6f9a1ae91
trahoangdev <trahoangdev@gmail.com>
2025-11-19 20:32:55 +0700

    Replace next/image with regular img tag for static export

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,6 +1,7 @@
-import NextImage, { ImageProps } from 'next/image'
-
-// For static export compatibility, we disable image optimization
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} unoptimized />
+// For static export compatibility, we use regular img tag instead of next/image
+const Image = ({ src, alt, width, height, ...rest }: any) => (
+  // eslint-disable-next-line @next/next/no-img-element
+  <img src={src} alt={alt} width={width} height={height} {...rest} />
+)
 
 export default Image
